c261d86b69c6702d06d0f266f14d52797e8076f4
jatinraju <jatin.raju795@gmail.com>
2024-10-07 13:02:12 +0530

    Initial Project Setup

---
+++ visual-crossing-service/src/main/java/com/wai/VisualCrossingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualCrossingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualCrossingServiceApplication.class, args);
+	}
+
+}
---
+++ visual-crossing-service/src/test/java/com/wai/VisualCrossingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualCrossingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
